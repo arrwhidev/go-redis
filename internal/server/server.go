487e50c57a1113ab0d0769432100048bb1a4f3fb
Arran White <arrwhidev@gmail.com>
2020-09-27 16:37:28 +0100

    print local ip when starting server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,12 +1,14 @@
 package server
 
 import (
-	"github.com/arrwhidev/go-redis/internal/request"
+	"fmt"
 	"log"
 	"net"
+	"strings"
+
+	"github.com/arrwhidev/go-redis/internal/request"
 )
 
-// Start a tcp server
 func Start() {
 	l, err := net.Listen("tcp4", ":6379")
 	if err != nil {
@@ -14,6 +16,9 @@ func Start() {
 		return
 	}
 	defer l.Close()
+
+	ip := getLocalIP()
+	fmt.Println("go-redis is listening at", ip+":6379")
 
 	for {
 		c, err := l.Accept()
@@ -25,4 +30,26 @@ func Start() {
 		r := request.NewRequest(c)
 		go r.Handle()
 	}
+}
+
+func getLocalIP() string {
+	ifaces, _ := net.Interfaces()
+	for _, i := range ifaces {
+		addrs, _ := i.Addrs()
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+
+			if strings.HasPrefix(ip.String(), "192.168") {
+				return ip.String()
+			}
+		}
+	}
+
+	return ""
 }
